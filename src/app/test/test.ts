4f26d1b0276ab248ce9b290d0c12518288baae55
yparenteau <yan.leigh@gmail.com>
2017-09-27 12:55:49 -0400

    enabled hrm

---
+++ src/app/test/test.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'test',
+  templateUrl: './test.html',
+  styleUrls: ['./test.scss']
+})
+
+export class TestComponent {
+  title = 'app';
+}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,70 @@
-import { enableProdMode } from '@angular/core';
+import {ApplicationRef, enableProdMode} from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {AppComponent} from './app/app.component';
+import {bootloader, createInputTransfer, createNewHosts, removeNgStyles} from '@angularclass/hmr';
+import {TestComponent} from './app/test/test';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TestComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+class MainModule {
+  constructor(public appRef: ApplicationRef) {}
+  hmrOnInit(store) {
+    if (!store || !store.state) return;
+    console.log('HMR store', store);
+    console.log('store.state.data:', store.state.data)
+    // inject AppStore here and update it
+    // this.AppStore.update(store.state)
+    if ('restoreInputValues' in store) {
+      store.restoreInputValues();
+    }
+    // change detection
+    this.appRef.tick();
+    delete store.state;
+    delete store.restoreInputValues;
+  }
+  hmrOnDestroy(store) {
+    var cmpLocation = this.appRef.components.map(cmp => cmp.location.nativeElement);
+    // recreate elements
+    store.disposeOldHosts = createNewHosts(cmpLocation)
+    // inject your AppStore and grab state then set it on store
+    // var appState = this.AppStore.get()
+    // store.state = {data: 'yolo'};
+    // store.state = Object.assign({}, appState)
+    // save input values
+    store.restoreInputValues  = createInputTransfer();
+    // remove styles
+    removeNgStyles();
+  }
+  hmrAfterDestroy(store) {
+    // display new elements
+    store.disposeOldHosts()
+    delete store.disposeOldHosts;
+    // anything you need done the component is removed
+  }
+}
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+function main() {
+  return platformBrowserDynamic().bootstrapModule(MainModule);
+}
+
+// boot on document ready
+bootloader(main);
